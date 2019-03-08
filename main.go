fe6c3cd3f53eb8c78c943f4a0b3288da59e4468c
Daniel Orf <dorf@suse.com>
2019-03-07 23:02:51 -0800

    Switched to gorilla/mux, added book submission page '/addbook'

--- main.go
+++ main.go
@@ -4,31 +4,66 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
-	"github.com/bmizerany/pat"
+	"github.com/gorilla/mux"
 )
 
+// Book is a struct!
 type Book struct {
 	Title  string
 	Author string
 	Length int
 }
 
-func main() {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(index))
-
-	log.Println("Listening...")
-	http.ListenAndServe(":3000", mux)
+// Books is a Global variable!  It will go away when we add a database.
+var Books = []Book{
+	{"Where the Wild Things Are", "Maurice Sendak", 45},
+	{"Cat in the Hat", "Doctor Seuss", 33},
+	{"Dictionary", "Steve", 666},
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", listBooks)
+	r.HandleFunc("/confirmation", confirmation)
+	r.HandleFunc("/addbook", addBookGet).Methods("GET")
+	r.HandleFunc("/addbook", addBookPost).Methods("POST")
+	// http.Handle("/", r)
 
-	books := []Book{
-		{"Where the Wild Things Are", "Maurice Sendak", 45},
-		{"Cat in the hat", "Doctor Seuss", 32},
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", r)
+}
+
+func listBooks(w http.ResponseWriter, r *http.Request) {
+	render(w, "templates/index.html", Books)
+}
+
+func addBookGet(w http.ResponseWriter, r *http.Request) {
+	render(w, "templates/addbook.html", nil)
+}
+
+func addBookPost(w http.ResponseWriter, r *http.Request) {
+	log.Println("Submitted...")
+
+	bookLength, _ := strconv.Atoi(strings.TrimSpace(r.FormValue("length")))
+
+	msg := &Book{
+		Title:  r.FormValue("title"),
+		Author: r.FormValue("author"),
+		Length: bookLength,
 	}
-	render(w, "templates/index.html", books)
+
+	Books = append(Books, *msg)
+
+	log.Println(msg)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func confirmation(w http.ResponseWriter, r *http.Request) {
+	render(w, "templates/confirmation.html", nil)
 }
 
 func render(w http.ResponseWriter, filename string, data interface{}) {
