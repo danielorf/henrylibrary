35bfdef38a362f5ac33e061659c2ec4617f15b4b
Daniel Orf <dorf@suse.com>
2019-03-10 22:41:24 -0700

    Added basic add/delete with in-memory storage

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,6 +13,7 @@ import (
 
 // Book is a struct!
 type Book struct {
+	ID     int
 	Title  string
 	Author string
 	Length int
@@ -19,18 +21,23 @@ type Book struct {
 
 // Books is a Global variable!  It will go away when we add a database.
 var Books = []Book{
-	{"Where the Wild Things Are", "Maurice Sendak", 45},
-	{"Cat in the Hat", "Doctor Seuss", 33},
-	{"Dictionary", "Steve", 666},
+	{1, "Where the Wild Things Are", "Maurice Sendak", 45},
+	{2, "Cat in the Hat", "Doctor Seuss", 33},
+	{3, "Dictionary", "Steve", 666},
+	{4, "Quicksiver", "Neil Stevenson", 100005},
 }
+
+var IDCounter = len(Books)
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", listBooks)
+
 	r.HandleFunc("/confirmation", confirmation)
 	r.HandleFunc("/addbook", addBookGet).Methods("GET")
 	r.HandleFunc("/addbook", addBookPost).Methods("POST")
-	// http.Handle("/", r)
+	r.HandleFunc("/deletebook/{id:[0-9]+}", deleteBook).Methods("GET")
+	r.HandleFunc("/", listBooks)
+	http.Handle("/", r)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", r)
@@ -46,10 +53,11 @@ func addBookGet(w http.ResponseWriter, r *http.Request) {
 
 func addBookPost(w http.ResponseWriter, r *http.Request) {
 	log.Println("Submitted...")
-
+	IDCounter++
 	bookLength, _ := strconv.Atoi(strings.TrimSpace(r.FormValue("length")))
 
 	msg := &Book{
+		ID:     IDCounter,
 		Title:  r.FormValue("title"),
 		Author: r.FormValue("author"),
 		Length: bookLength,
@@ -58,6 +66,29 @@ func addBookPost(w http.ResponseWriter, r *http.Request) {
 	Books = append(Books, *msg)
 
 	log.Println(msg)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	// w.WriteHeader(http.StatusOK)
+	fmt.Println(vars["id"])
+	id, _ := strconv.Atoi(vars["id"])
+
+	for i := 0; i < len(Books); i++ {
+		// Iterate through Book objects and remove Book if ID matches deleted ID
+		if id == Books[i].ID {
+			if i < len(Books)-1 {
+				Books = append(Books[:i], Books[i+1:]...)
+				break
+			} else {
+				Books = Books[:i]
+				break
+			}
+		}
+	}
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
